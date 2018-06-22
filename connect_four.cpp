215872f067633038e9c4ed4a027a675e28c132ac
Zachary Stence <zachstence@gmail.com>
2018-06-22 15:38:15 -0400

    Add framework for win checking
    - Change board design to 2D vector of Piece structs
    - Add Piece struct consisting of a display character and a map
      - Map is 4 key/value pairs - "near"
        - Keys are x,y offsets
        - Values are # of same pieces in that direction
    - Implement updating of "near" for placed pieces (still need to implement updating pieces already on the board)

--- connect_four.cpp
+++ connect_four.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 #include <utility>
+#include <stdexcept>
 
 using std::cin;
 using std::cout;
@@ -23,19 +24,49 @@ struct Player {
   bool won;
 };
 
-void displayBoard(vector<vector<char>> b) {
+struct Slot {
+  char disp = ' ';
+  map<pair<int, int>, int> near = {
+    { { -1,  1} , 0 },
+    { {  0,  1} , 0 },
+    { {  1,  1} , 0 },
+    { {  1,  0} , 0 }
+  };
+};
+
+void displayBoard(vector<vector<Slot>> b) {
   cout << "| 1 2 3 4 5 6 7 |" << endl;
   for (int r = 0; r < b[0].size(); r++) {
     cout << "| ";
     for (int c = 0; c < b.size(); c++) {
-      cout << b[c][r] << " ";
+      cout << b[c][r].disp << " ";
     }
     cout << "|" << endl;
   }
   cout << "|---------------|" << endl;
 }
 
-bool won(vector<vector<char>> b, char piece) {
+void displayDebug(vector<vector<Slot>> b) {
+  cout << "   |  0     1     2     3     4     5     6  |" << endl;
+  for (int r = 0; r < b[0].size(); r++) {
+    cout << "   |     |     |     |     |     |     |     |" << endl;
+    cout << r << "  ";
+    for (int c = 0; c < b.size(); c++) {
+      cout << "| " << " " << b[c][r].disp << b[c][r].near[ {1, 0}] << " ";
+    }
+    cout << "|";
+    cout << endl;
+    cout << "   ";
+    for (int c = 0; c < b.size(); c++) {
+      cout << "| " << b[c][r].near[ { -1, 1}] << b[c][r].near[ {0, 1}] << b[c][r].near[ {1, 1}] << " ";
+    }
+    cout << "|";
+    cout << endl;
+  }
+  cout << "   |-----------------------------------------|" << endl;
+}
+
+bool won(vector<vector<Slot>> b, char piece) {
   // for (int r = 0; r < b[0].size(); r++) {
   //   for (int c = 0; c < b.size(); c++) {
   //     if (b[c][r] == piece) {
@@ -43,19 +74,52 @@ bool won(vector<vector<char>> b, char piece) {
   //     }
   //   }
   // }
-  return true;
+  return false;
 }
 
-void placePiece(vector<vector<char>> &board, Player &p) {
-  int c;
+void placePiece(vector<vector<Slot>> &b, Player &p) {
+  int choice;
   cout << "Player " << p.id << ", enter a column to place a piece: ";
   while (true) {
-    cin >> c;
-    for (auto it = board[c - 1].rbegin(); it != board[c - 1].rend(); ++it) {
-      if (*it == ' ') {
-        *it = p.piece;
+    cin >> choice; // need to validate choice
+    int c = choice;
+    for (int r = ROWS - 1; r >= 0; r--) {
+      Slot *curr = &b[c][r];
+      if (curr->disp == ' ') {
+        curr->disp = p.piece;
 
-        p.won = won(board, p.piece);
+        // first initialize near counts of piece just placed
+        // loop through near counts
+        for (auto it = curr->near.begin(); it != curr->near.end(); ++it) {
+          // get direction and x, y offsets
+          auto direction = it->first;
+          int x = direction.first;
+          int y = direction.second;
+
+          try {
+            // if same piece in a direction, increment appropriate count
+            // use .at() to get exception instead of segfault
+            Slot *adj = &b.at(c + x).at(r + y);
+            if (adj->disp == curr->disp) {
+              curr->near[direction] = adj->near[direction] + 1;
+            }
+            // if out of bounds, just skip
+          } catch (const std::out_of_range& oor) {
+            continue;
+          }
+
+
+
+          // then initialize near counts of pieces in the opposite directions of what is in Slot.adj
+
+
+
+        }
+
+
+
+
+
 
         return;
       }
@@ -71,8 +135,8 @@ void displayColumn(vector<char> c) {
 }
 
 int main() {
-  vector<vector<char>> board (COLS, vector<char> (ROWS, ' '));
-  displayBoard(board);
+  vector<vector<Slot>> board (COLS, vector<Slot> (ROWS) );
+  displayDebug(board);
 
   Player p1 = {1, p1Piece, false};
   Player p2 = {2, p2Piece, false};
@@ -80,11 +144,11 @@ int main() {
   for (int count = 0; count < (ROWS * COLS); count += 2) {
     placePiece(board, p1);
     if (p1.won) break;
-    displayBoard(board);
+    displayDebug(board);
 
     placePiece(board, p2);
     if (p2.won) break;
-    displayBoard(board);
+    displayDebug(board);
   }
 
   int winner = p1.won ? 1 : p2.won ? 2 : 0;
